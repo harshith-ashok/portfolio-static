2f73e54e6b118e230f7af6384985826eca076bb9
Harshith Ashok <harshith@BMBP16.local>
2025-04-06 19:52:12 +0530

    base for initial deployment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { BlogComponent } from './blog/blog.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'blog', component: BlogComponent },
   { path: 'projects', component: ProjectsComponent },
 ];
